f989188d11038d5201e6202ae38aa7f9518cc045
Joy Rijith Chettiar <joy.rijith@gmail.com>
2024-06-27 05:06:28 +0000

    town_based_residential_filtering

---
+++ dbt/models/core/Town_based_Residential_filtering.sql
@@ -0,0 +1,25 @@
+
+WITH town_based_residential AS (
+    SELECT
+        town,
+        residential_type,
+        EXTRACT(YEAR FROM sold_date) AS sale_year,
+        ROUND(AVG(sale_amount), 0) AS avg_sale_amount
+    
+       from {{ ref('stg_real_estate_sale_connecticut_2008_to_2021') }}
+    WHERE
+        property_type = 'Residential'
+        AND EXTRACT(YEAR FROM sold_date) BETWEEN 2008 and 2021
+        GROUP BY
+        1, 2, 3
+    ORDER BY
+        1
+)
+
+SELECT
+    town AS County,
+    residential_type AS Dwelling_Type,
+    sale_year AS Sale_Year,
+    town_based_residential.avg_sale_amount AS Property_Type_Sale_Amount
+FROM
+    town_based_residential
