8655553e4864092017de7d80334df04f098dd5c8
Joy Chettiar <joy.rijith@gmail.com>
2024-06-28 03:43:06 +0000

    udpated code

--- dbt/models/core/Town_based_Residential_filtering.sql
+++ dbt/models/core/Town_based_Residential_filtering.sql
@@ -1,27 +1,20 @@
 
 WITH town_based_residential AS (
     SELECT
-        town,
-        address,
-        residential_type,
-        EXTRACT(YEAR FROM sold_date) AS sale_year,
-        ROUND(AVG(sale_amount), 0) AS avg_sale_amount
-    
+      *
        from {{ ref('stg_real_estate_sale_connecticut_2008_to_2021') }}
-    WHERE
-        property_type = 'Residential'
-        AND EXTRACT(YEAR FROM sold_date) BETWEEN 2008 and 2021
-        GROUP BY
-        1,3,4,2
-    ORDER BY
-        1
+
 )
 
-SELECT
-    town AS County,
-    address AS Property_location,
-    residential_type AS Dwelling_Type,
-    sale_year AS Sale_Year,
-    town_based_residential.avg_sale_amount AS Property_Type_Sale_Amount
-FROM
-    town_based_residential
+  select EXTRACT(YEAR FROM sold_date) AS sale_year,
+           town,
+           residential_type,
+           count(residential_type) as number_of_properties,
+           cast(avg(sale_amount) as integer) as avg_sale_price,
+           non_use_code,
+           count(non_use_code) as number_of_non_use_transactions
+
+    from town_based_residential
+    where residential_type IN ('Condo' , 'Single Family', 'Two Family', 'Three Family', 'Four Family', 'Apartments')
+    group by 1,2,3,6
+   
